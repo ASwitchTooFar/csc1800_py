d2e636dbdd733c06be8bdd479d0a07c9b7a3d163
Tom McDonald <tom.w.mcdonald@gmail.com>
2019-10-19 20:41:19 -0400

    Added unrelated function, all x-queries, removed echo for e-queries, reformated input parsing to account for nonexistant people.

--- family_tree.py
+++ family_tree.py
@@ -18,10 +18,6 @@
 import sys
 
 
-# Used only in testing to print dictionaries in a way which is readable.
-from pprint import pprint
-
-
 # Example person (key 1 is a list of parents, key 2 is spouse, key 3 is children):
 # family_tree = {
 #     person_name_string : {
@@ -33,8 +29,8 @@ from pprint import pprint
 family_tree = {}
 
 
-# Create the people (as sub-dictionaries) if they do not exist, set as each
-# other's spouse in either case.
+# Create the people (as sub-dictionaries) if they do not exist, set as each other's spouse
+# in either case.
 def event_marriage(person_1, person_2) :
     if person_1 not in family_tree :
         family_tree.update({person_1: {
@@ -57,8 +53,8 @@ def event_marriage(person_1, person_2) :
         family_tree[person_2][2] = person_1
 
 
-# Makes sure the parents exist and are married, then creates the child and
-# points the people at one another.
+# Makes sure the parents exist and are married, then creates the child and # points the
+# people at one another.
 def event_child(person_1, person_2, child) :
     event_marriage(person_1, person_2)
 
@@ -72,33 +68,60 @@ def event_child(person_1, person_2, child) :
     family_tree[person_2][3].append(child)
 
 
-#
+# Leverages existing 'W' query to check whether an individual is in the list of the
+# person's children.
 def x_query_child(child, person) :
-    print('Is ' + child + ' a child of ' + person + '?')
+    if child in w_query_child(person) :
+        print('Yes')
+
+    else :
+        print('No')
 
 
-#
+# Leverages existing 'W' query to check whether an individual is in the list of the
+# person's siblings.
 def x_query_sibling(sibling, person) :
-    print('Is ' + sibling + ' a sibling of ' + person + '?')
+    if sibling in w_query_sibling(person) :
+        print('Yes')
+
+    else :
+        print('No')
 
 
-#
+# Leverages existing 'W' query to check whether an individual is in the list of the
+# person's ancestors.
 def x_query_ancestor(ancestor, person) :
-    print('Is ' + ancestor + ' an ancestor of ' + person + '?')
+    if ancestor in w_query_ancestor(person, []) :
+        print('Yes')
+
+    else :
+        print('No')
 
 
-#
+# Leverages existing 'W' query to check whether an individual is in the list of the
+# person's cousins of a specific degree.
 def x_query_cousin(cousin, degree, person) :
-    print('Is ' + cousin + ' a ' + degree + ' degree cousin of ' + person + '?')
+    if cousin in w_query_cousin(person, degree) :
+        print('Yes')
 
-#
+    else :
+        print('No')
+
+
+# Leverages existing 'W' query to check whether an individual is in the list of those
+# unrelated to a person.
 def x_query_unrelated(non_relative, person) :
-    print('Is ' + non_relative + ' not a relative of ' + person + '?')
+    if non_relative in w_query_unrelated(person) :
+        print('Yes')
+
+    else :
+        print('No')
 
 
 # Returns a sorted list of the person's children from which duplicates have been removed.
 def w_query_child(person) :
     return sorted(family_tree[person][3])
+
 
 # Returns a sorted list of the person's siblings from which duplicates have been removed.
 def w_query_sibling(person) :
@@ -107,14 +130,18 @@ def w_query_sibling(person) :
     for parent in family_tree[person][1] :
         siblings = list(set(siblings + family_tree[parent][3]))
 
-    siblings.remove(person)
+    if person in siblings :
+        siblings.remove(person)
 
     return sorted(siblings)
 
 
-#TO DO: CHECK IF PERSON EXISTS
+# Returns all of a person's ancestors (recursively).
 def w_query_ancestor(person, list) :
-    if len(family_tree[person][1]) < 2: #if missing a parent
+    if person not in family_tree : #if person doesn't exist
+        return list
+
+    elif len(family_tree[person][1]) < 2: #if missing a parent
         return list
 
     else :
@@ -125,6 +152,65 @@ def w_query_ancestor(person, list) :
         list.append(family_tree[person][1][1])
 
     return list
+
+
+#final cousin method that returns ALL the nth zero_removed_cousins
+#this includes:
+#the nth cousins of the person's ancestors
+#the descendents of the persons zero_removed_cousins
+def w_query_cousin(person, degree):
+    zero_removed = []
+    ancestors = []
+    cousins = []
+
+    if (degree == 0) : #exception for when degree is zero
+        zero_removed += w_query_sibling(person)
+        for x in zero_removed:
+            cousins += (get_descendents(x,[])) #add decendents of zero removed
+
+        cousins += zero_removed
+        return cousins
+
+    #for any other degree above zero...code below
+
+    #get ancestors of person
+    ancestors += w_query_ancestor(person, [])
+    #get zero removed cousins of ancestor
+    zero_removed += get_zero_removed_cousins(person, degree)
+
+
+    #add the zero removed cousins of ALL the ancestors
+    for ancestor in ancestors:
+        cousins += get_zero_removed_cousins(ancestor, degree)
+
+    for x in zero_removed:
+        cousins += get_descendents(x,[])
+
+    cousins += zero_removed
+    return cousins;
+
+
+# Finds all ancestors of a person then all decendants of those ancestors. After removing those from
+# the list of all people, only those whom are unrelated remain.
+def w_query_unrelated(person) :
+    unrelated = list(family_tree.keys())
+    ancestors = list(set(w_query_ancestor(person, [])))
+
+    for ancestor in ancestors :
+        descendants = get_descendents(ancestor, [])
+
+        try:
+            unrelated.remove(ancestor)
+        except ValueError:
+            pass
+
+        for descendant in descendants :
+            try :
+                unrelated.remove(descendant)
+            except ValueError :
+                pass
+
+    return sorted(unrelated)
 
 
 #this method is used in get_zero_removed_cousins
@@ -144,6 +230,7 @@ def get_grandparents(person, level, list) :
         get_grandparents(family_tree[person][1][1], level -1, list)
 
     return list
+
 
 #this method is used in get_zero_removed_cousins
 #returns all people on level zero, starting from the selected grandparent
@@ -186,18 +273,15 @@ def get_zero_removed_cousins (starterPerson, degree) :
     #obtain the needed (great)* grandparents according to the degree
     grandparents += get_grandparents(starterPerson, degree, [])
 
-
     #Flag people who have starterPerson as a descendent
     for i in grandparents:
         for j in w_query_child(i):
             if (starterPerson in get_descendents(j, [])) :
                 flagged.append(j)
 
-
     #add to descendents_at_zero_level
     for x in grandparents:
         descendents_at_zero_level += (get_descendents_at_zero_level(x, degree, []))
-
 
     #only add people who DO NOT have a flagged person as an ancestor
     zero_removed_cousins = []
@@ -208,113 +292,115 @@ def get_zero_removed_cousins (starterPerson, degree) :
     return zero_removed_cousins
 
 
-#final cousin method that returns ALL the nth zero_removed_cousins
-#this includes:
-#the nth cousins of the person's ancestors
-#the descendents of the persons zero_removed_cousins
-def w_query_cousin(person, degree):
-    zero_removed = []
-    ancestors = []
-    cousins = []
-
-    if (degree == 0) : #exception for when degree is zero
-        zero_removed += w_query_sibling(person)
-        for x in zero_removed:
-            cousins += (get_descendents(x,[])) #add decendents of zero removed
-
-        cousins += zero_removed
-        return cousins
-
-    #for any other degree above zero...code below
-
-    #get ancestors of person
-    ancestors += w_query_ancestor(person, [])
-    #get zero removed cousins of ancestor
-    zero_removed += get_zero_removed_cousins(person, degree)
-
-
-    #add the zero removed cousins of ALL the ancestors
-    for ancestor in ancestors:
-        cousins += get_zero_removed_cousins(ancestor, degree)
-
-    for x in zero_removed:
-        cousins += get_descendents(x,[])
-
-    cousins += zero_removed
-    return cousins;
-
-#
-def w_query_unrelated(person) :
-    return ['List all people unrelated to ' + person + '.']
-
-
-
-# HELPER METHODS
-
-
-
 # Determines the type of query based on contents and length of the split-up string.
 def parse_line(split_line) :
     if split_line[0] == 'E' :
         if len(split_line) == 3 :
             event_marriage(split_line[1], split_line[2])
+
         else :
             event_child(split_line[1], split_line[2], split_line[3])
 
     if split_line[0] == 'X' :
         if (split_line[2] == 'child') :
-            x_query_child(split_line[1], split_line[3])
+            if split_line[3] in family_tree :
+                x_query_child(split_line[1], split_line[3])
+                print()
+
+            else:
+                print(split_line[3] + ' does not exist in the family tree.\n')
 
         elif (split_line[2] == 'sibling') :
-            x_query_sibling(split_line[1], split_line[3])
+            if split_line[3] in family_tree :
+                x_query_sibling(split_line[1], split_line[3])
+                print()
+
+            else:
+                print(split_line[3] + ' does not exist in the family tree.\n')
 
         elif (split_line[2] == 'ancestor') :
-            print("test")
-            #ancestors = list(set(x_query_ancestor(split_line[1], split_line[3])))
-            #print(ancestors)
+            if split_line[3] in family_tree :
+                x_query_ancestor(split_line[1], split_line[3])
+                print()
 
-        elif (split_line[2] == 'cousin'):
-            x_query_cousin(split_line[1], int(split_line[3]), split_line[4])
+            else:
+                print(split_line[3] + ' does not exist in the family tree.\n')
+
+        elif (split_line[2] == 'cousin') :
+            if split_line[4] in family_tree :
+                x_query_cousin(split_line[1], int(split_line[3]), split_line[4])
+                print()
+
+            else:
+                print(split_line[4] + ' does not exist in the family tree.\n')
 
         else :
-            x_query_unrelated(split_line[1], split_line[3])
+            if split_line[3] in family_tree :
+                x_query_unrelated(split_line[1], split_line[3])
+                print()
+
+            else:
+                print(split_line[3] + ' does not exist in the family tree.\n')
 
     if split_line[0] == 'W' :
-        if (split_line[1] == 'child'):
-            print(*w_query_child(split_line[2]), sep='\n')
+        if (split_line[1] == 'child') :
+            if split_line[2] in family_tree :
+                print(*w_query_child(split_line[2]), sep='\n')
+                print()
 
-        elif (split_line[1] == 'sibling'):
-            print(*w_query_sibling(split_line[2]), sep='\n')
+            else :
+                print(split_line[2] + ' does not exist in the family tree.\n')
 
-        elif (split_line[1] == 'ancestor'):
-            print("List the ancestors of " + split_line[2])
-            ancestors = w_query_ancestor(split_line[2], [])
-            #REMOVE DUPLICATES
-            ancestors = sorted(list(dict.fromkeys(ancestors)))
-            print(*ancestors, sep='\n')
+        elif (split_line[1] == 'sibling') :
+            if split_line[2] in family_tree :
+                print(*w_query_sibling(split_line[2]), sep='\n')
+                print()
 
+            else :
+                print(split_line[2] + ' does not exist in the family tree.\n')
 
-        elif (split_line[1] == 'cousin'):
-            cousins = (w_query_cousin(split_line[3], int(split_line[2])))
-            #REMOVE DUPLICATES
-            cousins = sorted(list(dict.fromkeys(cousins)))
-            print(*cousins, sep='\n')
+        elif (split_line[1] == 'ancestor') :
+            if split_line[2] in family_tree :
+                ancestors = w_query_ancestor(split_line[2], [])
+                #REMOVE DUPLICATES
+                ancestors = sorted(list(dict.fromkeys(ancestors)))
+                print(*ancestors, sep='\n')
+                print()
+
+            else :
+                print(split_line[2] + ' does not exist in the family tree.\n')
+
+        elif (split_line[1] == 'cousin') :
+            if split_line[3] in family_tree :
+                cousins = (w_query_cousin(split_line[3], int(split_line[2])))
+                #REMOVE DUPLICATES
+                cousins = sorted(list(dict.fromkeys(cousins)))
+                print(*cousins, sep='\n')
+                print()
+
+            else :
+                print(split_line[3] + ' does not exist in the family tree.\n')
 
         else :
-            print(*w_query_unrelated(split_line[2]), sep='\n')
+            if split_line[2] in family_tree :
+                print(*w_query_unrelated(split_line[2]), sep='\n')
+                print()
 
+            else :
+                print(split_line[2] + ' does not exist in the family tree.\n')
 
 # Iterates over standard in, determines the type of event / query for each line,
 # echoes the line back to the user, and then calls the appropriate method.
 def main():
     for line in sys.stdin:
         current_line = line.rstrip()
-        print(current_line)
-        parse_line(current_line.split())
-        print()
+        split_line = current_line.split()
 
+        if split_line[0] != 'E' :
+            print(current_line)
 
-    pprint(family_tree)
+        parse_line(split_line)
+
 
 if __name__ == '__main__':
     main()
